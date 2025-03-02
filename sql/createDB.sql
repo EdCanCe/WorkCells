b1206614bdac748818c5d0ae9979a43e68995b6e
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-03-02 16:11:03 -0600

    Second iteration, more questions on some tables.

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -1,4 +1,3 @@
-BEGIN -- No es totalmente necesario detallar el begin
 
 CREATE TABLE holiday (
     holidayDate TINYINT NOT NULL PRIMARY KEY,
@@ -16,21 +15,30 @@ CREATE TABLE role (
     title VARCHAR(60) NOT NULL
 );
 
--- La relación de privileges y role es de N a N, se ocupa crear una tabla llamada rolePrivileges en donde tengas las FK de ambas
+-- ¿Qué atributos debería de llevar? xD
+-- En el diagrama no tiene ningúno.
+
+CREATE TABLE rolePrivilages (
+    
+    roleIDFK TINYINT NOT NULL,
+    CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID)
+    privilegesIDFK TINYINT NOT NULL,
+    CONSTRAINT privilegesIDFK FOREIGN KEY (privilegesIDFK) REFERENCES privileges(privilegesID)
+)
 
 CREATE TABLE user (
-    userID BIGINT NOT NULL PRIMARY KEY, -- Con un mediumInt podría jalar
+    userID MEDIUMINT NOT NULL PRIMARY KEY,
     zipCode TINYINT NOT NULL,
     houseNumber TINYINT NOT NULL,
     streetName VARCHAR(100),
     passwd VARCHAR(70) NOT NULL,
     passwdFlag BOOLEAN NOT NULL,
-    mail VARCHAR(50) NOT NULL, -- Cambiar a 70 caracteres quizás
+    mail VARCHAR(70) NOT NULL,
     fSurname VARCHAR(50) NOT NULL,
-    mSurname VARCHAR(50), -- BTW, ésta es not null pq no todas las personas tienen 2 apellidos, ejemplo: Estadounidenses, ellos tienen solo 1
+    mSurname VARCHAR(50),
     brithName VARCHAR(50) NOT NULL,
-    workModality VARCHAR(50) NOT NULL, -- Cambiar a tinyInt, 0 = presencial, 1 = híbrida, 2 = homeOffice
-    workStatus VARCHAR(50) NOT NULL, -- Es booleana
+    workModality TINYINT NOT NULL, -- 0 = presencial, 1 = híbrida, 2 = homeOffice
+    workStatus BOOLEAN NOT NULL, -- Le había puesto varchar por si acaso fuera de que hubiera un tercer caso jsjs, lo dejo como boolean
     roleIDFK TINYINT NOT NULL,
     CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID)
 );
@@ -39,7 +47,7 @@ CREATE TABLE workStatus (
     workStatusID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     startDate DATE NOT NULL,
     endDate DATE,
-    userIDFK BIGINT NOT NULL, -- Si se cambia el id del usuario a mediumInt, éste también se tiene que tambiar a int
+    userIDFK MEDIUMINT NOT NULL,
     CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
 );
 
@@ -64,7 +72,7 @@ CREATE TABLE kpi (
     monthDuration TINYINT NOT NULL,
     departamentIDFK TINYINT NOT NULL,
     CONSTRAINT departamentIDFK FOREIGN KEY (departamentIDFK) REFERENCES departament(departamentID),
-    userIDFK BIGINT NOT NULL, -- Si se cambia el id del usuario a mediumInt, éste también se tiene que tambiar a int
+    userIDFK MEDIUMINT NOT NULL,
     CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
 );
 
@@ -78,7 +86,7 @@ CREATE TABLE evidence (
 
 CREATE TABLE evidenceMedia (
     evidenceMediaID INT NOT NULL PRIMARY KEY,
-    mediaLink VARCHAR(255), -- También es not null
+    mediaLink VARCHAR(255) NOT NULL,
     evidenceIDFK INT NOT NULL,
     CONSTRAINT evidenceIDFK FOREIGN KEY (evidenceIDFK) REFERENCES evidence(evidenceID)
 );
@@ -90,44 +98,40 @@ CREATE TABLE vactions (
     HRStatus BOOLEAN NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    userIDFK BIGINT NOT NULL, -- Si se cambia el id del usuario a mediumInt, éste también se tiene que tambiar a int
+    userIDFK MEDIUMINT NOT NULL,
     CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE absence (
-    absenceID TINYINT NOT NULL PRIMARY KEY, -- Se ocuparía un int yo creo
-    reason VARCHAR(300), -- si es menor a 3 días, especificar de forma automática. | Eso se especifica en el JavaScript
+    absenceID INT NOT NULL PRIMARY KEY,
+    reason VARCHAR(300), -- si es menor a 3 días, especificar de forma automática. | Eso se especifica en el JavaScript | Simon, pero dejar como comentario para que, cuando alguien lo revise vea algo tipo "Sin razón. Menor a 3 días." o algo por el estilo.
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    justified BOOLEAN NOT NULL, -- mmm revisar si esto no está un poco de dictador no? xd | Pues es pq el lider de departamento "aprueba" las ausencias, no?
-    userIDFK BIGINT NOT NULL, -- Si se cambia el id del usuario a mediumInt, éste también se tiene que tambiar a int
+    justified BOOLEAN NOT NULL, -- mmm revisar si esto no está un poco de dictador no? xd | Pues es pq el lider de departamento "aprueba" las ausencias, no? | Pero por ejemplo, q tal si es la ausencia por enfermedad ¿se la va a negar?
+    userIDFK MEDIUMINT NOT NULL,
     CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE absenceMedia (
-    absenceMediaID TINYINT NOT NULL PRIMARY KEY, -- Se ocuparía cambiar a int yo creo
-    mediaLink VARCHAR(255), -- Es not null, en caso de que no se ocupe un archivo multimedia se omitiría la creación de la tabla
+    absenceMediaID INT NOT NULL PRIMARY KEY,
+    mediaLink VARCHAR(255), -- Es not null, en caso de que no se ocupe un archivo multimedia se omitiría la creación de la tabla | mmm y que tal si la razón es porque se le murió alguien? creo q estaría gacho que en ese caso deba de presentar alguna evidencia xD, por eso no le puse el not null
     absenceIDFK BIGINT NOT NULL,
     CONSTRAINT absenceIDFK FOREIGN KEY (absenceIDFK) REFERENCES absence(absenceIDFK)
 );
 
 CREATE TABLE oneOnOne (
-    oneOnOneID TINYINT NOT NULL PRIMARY KEY, -- Se ocuparía cambiar a int yo creo
+    oneOnOneID INT NOT NULL PRIMARY KEY,
     meetingDate TIMESTAMP NOT NULL,
-    expectedTime TIME NOT NULL, -- esto hace referencia a cuanto tiempo va a durar la junta? | Yessir, la cantidad de minutos, por lo mismo cambiar a TinyInt
-    userIDFK BIGINT NOT NULL, -- Si se cambia el id del usuario a mediumInt, éste también se tiene que tambiar a int
+    expectedTime TINYINT NOT NULL, -- esto hace referencia a cuanto tiempo va a durar la junta? | Yessir, la cantidad de minutos, por lo mismo cambiar a TinyInt | le puse TIME porque, supongo yo, no debería de durar más de 1 hora, aunque en cualquier caso, aunque dure más de una hora, aún así el tipo de dato podría ser TIME no? de todas formas ya lo puse como int jsjs
+    userIDFK MEDIUMINT NOT NULL,
     CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
 )
 
 CREATE TABLE oneOnOneQuestion (
     questionID TINYINT NOT NULL PRIMARY KEY,
     question VARCHAR(300) NOT NULL,
-    oneOnOneIDFK TINYINT NOT NULL, -- La relación con el One on One va en la tabla de oneOnOneAnswers
-    CONSTRAINT oneOnOneIDFK FOREIGN KEY (oneOnOneIDFK) REFERENCES oneOnOne(oneOnOneID)
 );
 
--- Lo siguiente es correcto? es decir, si son dos llaves foraneas o debería de ser una llave compuesta? | Está bien con las llaves foráneas
--- y bueno, en general está bien? aunque creo que no pq no puse llave primaria jsjsjs | No ocupan llevar todas llaves primarias
 CREATE TABLE oneOnOneAnswer (
     answer VARCHAR(300) NOT NULL,
     oneOnOneIDFK BIGINT NOT NULL,
