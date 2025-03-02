8979caf60f221644a2b26bcf6294eabd12afd815
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-03-01 23:30:13 -0600

    Create DB test on PHPMYADMIN

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -46,48 +46,12 @@ CREATE TABLE evidence (
     summary VARCHAR(300) NOT NULL,
     uploadDate TIMESTAMP NOT NULL,
     kpiIDFK INT NOT NULL,
-    CONSTRAINT kpiIDFK FOREIGN KEY (kpiID) REFERENCES kpi(kpiID),
+    CONSTRAINT kpiIDFK FOREIGN KEY (kpiIDFK) REFERENCES kpi(kpiID)
 );
 
 CREATE TABLE evidenceMedia (
     evidenceMediaID INT NOT NULL PRIMARY KEY,
-    mediaLink VARCHAR(255)
-    evidenceIDFK INT NOT NULL PRIMARY KEY,
-    CONSTRAINT evidenceIDFK FOREIGN KEY (evidenceID) REFERENCES evidence(evidenceID)
+    mediaLink VARCHAR(255),
+    evidenceIDFK INT NOT NULL,
+    CONSTRAINT evidenceIDFK FOREIGN KEY (evidenceIDFK) REFERENCES evidence(evidenceID)
 );
-
-
-
-END
-
--- create table User(
---     ID_User bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     Password_User varchar(220) NOT NULL,
---     Name_User varchar(25) NOT NULL,
---     LastName_User varchar(60),
---     BirthDate_User date NOT NULL,
---     Username_User varchar(20) NOT NULL,
---     Administrator_User tinyint NOT NULL,
---     Description_User varchar(250)
--- );
-
--- create table Routine(
---     ID_Routine bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     FKID_User_Routine bigint NOT NULL,
---     CONSTRAINT FKID_User_Routine FOREIGN KEY (FKID_User_Routine) REFERENCES User(ID_User),
---     Name_Routine varchar(30) NOT NULL
--- );
-
--- create table Exercise(
---     ID_Exercise bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     Name_Exercise varchar(30) NOT NULL,
---     Description_Exercise text(500) NOT NULL
--- );
-
--- create table RoutineExercise (
---     ID_RoutineExercise bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     FKID_Routine_RoutineExercise bigint NOT NULL,
---     CONSTRAINT FKID_Routine_RoutineExercise FOREIGN KEY (FKID_Routine_RoutineExercise) REFERENCES Routine(ID_Routine),
---     FK_Exercise_RoutineExercise bigint NOT NULL,
---     CONSTRAINT FK_Exercise_RoutineExercise FOREIGN KEY (FK_Exercise_RoutineExercise) REFERENCES Exercise(ID_Exercise)
--- );
