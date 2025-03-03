211cf84e1e16635ea532f670cc803b85cbd43bb7
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-03-02 21:22:43 -0600

    Squashed commit of the following:
    commit cc46fe8613c69e023e1a124283f6d5cd353d25e1
    Author: 🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
    Date:   Sun Mar 2 19:14:33 2025 -0600
    
        Base de datos en SQL funcional.
    
    commit b1206614bdac748818c5d0ae9979a43e68995b6e
    Author: Mauricio Olguin <mauolguisanchez@gmail.com>
    Date:   Sun Mar 2 16:11:03 2025 -0600
    
        Second iteration, more questions on some tables.
    
    commit 6320d22f6ae62ebc709af03b7b3117f40bdd7a7a
    Author: 🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
    Date:   Sun Mar 2 15:44:21 2025 -0600
    
        Comentarios hacia la base de datos
    
    commit ce93a1ee000f9c2d7ec8bfdd85fe5cc86829d8a6
    Author: Mauricio Olguin <mauolguisanchez@gmail.com>
    Date:   Sun Mar 2 01:12:21 2025 -0600
    
        All tables written, waiting Edmundo for updates
    
    commit 799f92d5d4831cdb41e0e2f25d85efb0ea6e157b
    Author: 🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
    Date:   Sat Mar 1 23:30:13 2025 -0600
    
        Create DB test on PHPMYADMIN
    
    commit 99996c50e83c2598e879ba8f53b9ebccf321c691
    Author: Mauricio Olguin <mauolguisanchez@gmail.com>
    Date:   Sat Mar 1 23:26:04 2025 -0600
    
        Begining sql of data base

---
+++ sql/createDB.sql
@@ -0,0 +1,144 @@
+
+CREATE TABLE holiday (
+    holidayDate TINYINT NOT NULL PRIMARY KEY,
+    title VARCHAR(100)
+);
+
+CREATE TABLE privileges (
+    privilegesID TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(60) NOT NULL,
+    summary VARCHAR(300)
+);
+
+CREATE TABLE role (
+    roleID TINYINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE rolePrivilages (
+    roleIDFK TINYINT NOT NULL,
+    CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID),
+    privilegesIDFK TINYINT NOT NULL,
+    CONSTRAINT privilegesIDFK FOREIGN KEY (privilegesIDFK) REFERENCES privileges(privilegesID)
+);
+
+CREATE TABLE user (
+    userID MEDIUMINT NOT NULL PRIMARY KEY,
+    zipCode TINYINT NOT NULL,
+    houseNumber TINYINT NOT NULL,
+    streetName VARCHAR(100),
+    passwd VARCHAR(70) NOT NULL,
+    passwdFlag BOOLEAN NOT NULL,
+    mail VARCHAR(70) NOT NULL,
+    fSurname VARCHAR(50) NOT NULL,
+    mSurname VARCHAR(50),
+    birthName VARCHAR(50) NOT NULL,
+    workModality TINYINT NOT NULL,
+    workStatus BOOLEAN NOT NULL,
+    userRoleIDFK TINYINT NOT NULL,
+    CONSTRAINT userRoleIDFK FOREIGN KEY (userRoleIDFK) REFERENCES role(roleID)
+);
+
+CREATE TABLE workStatus (
+    workStatusID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    startDate DATE NOT NULL,
+    endDate DATE,
+    userStatusIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT userStatusIDFK FOREIGN KEY (userStatusIDFK) REFERENCES user(userID)
+);
+
+CREATE TABLE enterprise (
+    enterpriseID TINYINT NOT NULL PRIMARY KEY,
+    title VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE departament (
+    departamentID TINYINT NOT NULL PRIMARY KEY,
+    title VARCHAR(60) NOT NULL,
+    enterpriseIDFK TINYINT NOT NULL,
+    CONSTRAINT enterpriseIDFK FOREIGN KEY (enterpriseIDFK) REFERENCES enterprise(enterpriseID)
+);
+
+CREATE TABLE userDepartment (
+    userIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID),
+    departamentIDFK TINYINT NOT NULL,
+    CONSTRAINT departamentIDFK FOREIGN KEY (departamentIDFK) REFERENCES departament(departamentID)
+);
+
+CREATE TABLE kpi (
+    kpiID INT NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    creationDate DATE NOT NULL,
+    progress TINYINT NOT NULL,
+    goal VARCHAR(300) NOT NULL,
+    monthDuration TINYINT NOT NULL,
+    KpiDepartamentIDFK TINYINT NOT NULL,
+    CONSTRAINT KpiDepartamentIDFK FOREIGN KEY (KpiDepartamentIDFK) REFERENCES departament(departamentID),
+    kpiUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT kpiUserIDFK FOREIGN KEY (kpiUserIDFK) REFERENCES user(userID)
+);
+
+CREATE TABLE evidence (
+    evidenceID INT NOT NULL PRIMARY KEY,
+    summary VARCHAR(300) NOT NULL,
+    uploadDate TIMESTAMP NOT NULL,
+    evidenceKpiIDFK INT NOT NULL,
+    CONSTRAINT evidenceKpiIDFK FOREIGN KEY (evidenceKpiIDFK) REFERENCES kpi(kpiID)
+);
+
+CREATE TABLE evidenceMedia (
+    evidenceMediaID INT NOT NULL PRIMARY KEY,
+    mediaLink VARCHAR(255) NOT NULL,
+    evidenceIDFK INT NOT NULL,
+    CONSTRAINT evidenceIDFK FOREIGN KEY (evidenceIDFK) REFERENCES evidence(evidenceID)
+);
+
+CREATE TABLE vactions (
+    vacationsID INT NOT NULL PRIMARY KEY,
+    leaderStatus BOOLEAN NOT NULL,
+    reason VARCHAR(300),
+    HRStatus BOOLEAN NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    vacationUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT vacationUserIDFK FOREIGN KEY (vacationUserIDFK) REFERENCES user(userID)
+);
+
+CREATE TABLE absence (
+    absenceID INT NOT NULL PRIMARY KEY,
+    reason VARCHAR(300),
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    justified BOOLEAN NOT NULL,
+    absenceUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT absenceUserIDFK FOREIGN KEY (absenceUserIDFK) REFERENCES user(userID)
+);
+
+CREATE TABLE absenceMedia (
+    absenceMediaID INT NOT NULL PRIMARY KEY,
+    mediaLink VARCHAR(255) NOT NULL,
+    absenceIDFK INT NOT NULL,
+    CONSTRAINT absenceIDFK FOREIGN KEY (absenceIDFK) REFERENCES absence(absenceID)
+);
+
+CREATE TABLE oneOnOne (
+    oneOnOneID INT NOT NULL PRIMARY KEY,
+    meetingDate TIMESTAMP NOT NULL,
+    expectedTime TINYINT NOT NULL,
+    oneOnOneuserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT oneOnOneuserIDFK FOREIGN KEY (oneOnOneuserIDFK) REFERENCES user(userID)
+);
+
+CREATE TABLE oneOnOneQuestion (
+    questionID TINYINT NOT NULL PRIMARY KEY,
+    question VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE oneOnOneAnswer (
+    answer VARCHAR(300) NOT NULL,
+    oneOnOneIDFK INT NOT NULL,
+    CONSTRAINT oneOnOneIDFK FOREIGN KEY (oneOnOneIDFK) REFERENCES oneOnOne(oneOnOneID),
+    questionIDFK TINYINT NOT NULL,
+    CONSTRAINT questionIDFK FOREIGN KEY (questionIDFK) REFERENCES oneOnOneQuestion(questionID)
+);
