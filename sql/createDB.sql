3322779c5c7e4d058b0e20c11835056bd8a51be1
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-03-02 19:14:33 -0600

    Base de datos en SQL funcional.

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -15,16 +15,12 @@ CREATE TABLE role (
     title VARCHAR(60) NOT NULL
 );
 
--- ¿Qué atributos debería de llevar? xD
--- En el diagrama no tiene ningúno.
-
 CREATE TABLE rolePrivilages (
-    
     roleIDFK TINYINT NOT NULL,
-    CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID)
+    CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID),
     privilegesIDFK TINYINT NOT NULL,
     CONSTRAINT privilegesIDFK FOREIGN KEY (privilegesIDFK) REFERENCES privileges(privilegesID)
-)
+);
 
 CREATE TABLE user (
     userID MEDIUMINT NOT NULL PRIMARY KEY,
@@ -36,19 +32,19 @@ CREATE TABLE user (
     mail VARCHAR(70) NOT NULL,
     fSurname VARCHAR(50) NOT NULL,
     mSurname VARCHAR(50),
-    brithName VARCHAR(50) NOT NULL,
-    workModality TINYINT NOT NULL, -- 0 = presencial, 1 = híbrida, 2 = homeOffice
-    workStatus BOOLEAN NOT NULL, -- Le había puesto varchar por si acaso fuera de que hubiera un tercer caso jsjs, lo dejo como boolean
-    roleIDFK TINYINT NOT NULL,
-    CONSTRAINT roleIDFK FOREIGN KEY (roleIDFK) REFERENCES role(roleID)
+    birthName VARCHAR(50) NOT NULL,
+    workModality TINYINT NOT NULL,
+    workStatus BOOLEAN NOT NULL,
+    userRoleIDFK TINYINT NOT NULL,
+    CONSTRAINT userRoleIDFK FOREIGN KEY (userRoleIDFK) REFERENCES role(roleID)
 );
 
 CREATE TABLE workStatus (
     workStatusID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     startDate DATE NOT NULL,
     endDate DATE,
-    userIDFK MEDIUMINT NOT NULL,
-    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
+    userStatusIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT userStatusIDFK FOREIGN KEY (userStatusIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE enterprise (
@@ -63,6 +59,13 @@ CREATE TABLE departament (
     CONSTRAINT enterpriseIDFK FOREIGN KEY (enterpriseIDFK) REFERENCES enterprise(enterpriseID)
 );
 
+CREATE TABLE userDepartment (
+    userIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID),
+    departamentIDFK TINYINT NOT NULL,
+    CONSTRAINT departamentIDFK FOREIGN KEY (departamentIDFK) REFERENCES departament(departamentID)
+);
+
 CREATE TABLE kpi (
     kpiID INT NOT NULL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -70,18 +73,18 @@ CREATE TABLE kpi (
     progress TINYINT NOT NULL,
     goal VARCHAR(300) NOT NULL,
     monthDuration TINYINT NOT NULL,
-    departamentIDFK TINYINT NOT NULL,
-    CONSTRAINT departamentIDFK FOREIGN KEY (departamentIDFK) REFERENCES departament(departamentID),
-    userIDFK MEDIUMINT NOT NULL,
-    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
+    KpiDepartamentIDFK TINYINT NOT NULL,
+    CONSTRAINT KpiDepartamentIDFK FOREIGN KEY (KpiDepartamentIDFK) REFERENCES departament(departamentID),
+    kpiUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT kpiUserIDFK FOREIGN KEY (kpiUserIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE evidence (
     evidenceID INT NOT NULL PRIMARY KEY,
     summary VARCHAR(300) NOT NULL,
     uploadDate TIMESTAMP NOT NULL,
-    kpiIDFK INT NOT NULL,
-    CONSTRAINT kpiIDFK FOREIGN KEY (kpiIDFK) REFERENCES kpi(kpiID)
+    evidenceKpiIDFK INT NOT NULL,
+    CONSTRAINT evidenceKpiIDFK FOREIGN KEY (evidenceKpiIDFK) REFERENCES kpi(kpiID)
 );
 
 CREATE TABLE evidenceMedia (
@@ -98,43 +101,43 @@ CREATE TABLE vactions (
     HRStatus BOOLEAN NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    userIDFK MEDIUMINT NOT NULL,
-    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
+    vacationUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT vacationUserIDFK FOREIGN KEY (vacationUserIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE absence (
     absenceID INT NOT NULL PRIMARY KEY,
-    reason VARCHAR(300), -- si es menor a 3 días, especificar de forma automática. | Eso se especifica en el JavaScript | Simon, pero dejar como comentario para que, cuando alguien lo revise vea algo tipo "Sin razón. Menor a 3 días." o algo por el estilo.
+    reason VARCHAR(300),
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    justified BOOLEAN NOT NULL, -- mmm revisar si esto no está un poco de dictador no? xd | Pues es pq el lider de departamento "aprueba" las ausencias, no? | Pero por ejemplo, q tal si es la ausencia por enfermedad ¿se la va a negar?
-    userIDFK MEDIUMINT NOT NULL,
-    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
+    justified BOOLEAN NOT NULL,
+    absenceUserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT absenceUserIDFK FOREIGN KEY (absenceUserIDFK) REFERENCES user(userID)
 );
 
 CREATE TABLE absenceMedia (
     absenceMediaID INT NOT NULL PRIMARY KEY,
-    mediaLink VARCHAR(255), -- Es not null, en caso de que no se ocupe un archivo multimedia se omitiría la creación de la tabla | mmm y que tal si la razón es porque se le murió alguien? creo q estaría gacho que en ese caso deba de presentar alguna evidencia xD, por eso no le puse el not null
-    absenceIDFK BIGINT NOT NULL,
-    CONSTRAINT absenceIDFK FOREIGN KEY (absenceIDFK) REFERENCES absence(absenceIDFK)
+    mediaLink VARCHAR(255) NOT NULL,
+    absenceIDFK INT NOT NULL,
+    CONSTRAINT absenceIDFK FOREIGN KEY (absenceIDFK) REFERENCES absence(absenceID)
 );
 
 CREATE TABLE oneOnOne (
     oneOnOneID INT NOT NULL PRIMARY KEY,
     meetingDate TIMESTAMP NOT NULL,
-    expectedTime TINYINT NOT NULL, -- esto hace referencia a cuanto tiempo va a durar la junta? | Yessir, la cantidad de minutos, por lo mismo cambiar a TinyInt | le puse TIME porque, supongo yo, no debería de durar más de 1 hora, aunque en cualquier caso, aunque dure más de una hora, aún así el tipo de dato podría ser TIME no? de todas formas ya lo puse como int jsjs
-    userIDFK MEDIUMINT NOT NULL,
-    CONSTRAINT userIDFK FOREIGN KEY (userIDFK) REFERENCES user(userID)
-)
+    expectedTime TINYINT NOT NULL,
+    oneOnOneuserIDFK MEDIUMINT NOT NULL,
+    CONSTRAINT oneOnOneuserIDFK FOREIGN KEY (oneOnOneuserIDFK) REFERENCES user(userID)
+);
 
 CREATE TABLE oneOnOneQuestion (
     questionID TINYINT NOT NULL PRIMARY KEY,
-    question VARCHAR(300) NOT NULL,
+    question VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE oneOnOneAnswer (
     answer VARCHAR(300) NOT NULL,
-    oneOnOneIDFK BIGINT NOT NULL,
+    oneOnOneIDFK INT NOT NULL,
     CONSTRAINT oneOnOneIDFK FOREIGN KEY (oneOnOneIDFK) REFERENCES oneOnOne(oneOnOneID),
     questionIDFK TINYINT NOT NULL,
     CONSTRAINT questionIDFK FOREIGN KEY (questionIDFK) REFERENCES oneOnOneQuestion(questionID)
