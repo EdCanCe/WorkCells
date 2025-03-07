db36523024cde4ca37b78bdc2133930a6c3aa3f0
YaelChar417 <A01711111@tec.mx>
2025-03-07 16:44:53 -0600

    Update: ruta para departamentos /add y /check creadas

--- navegacion/controllers/department.controller.js
+++ navegacion/controllers/department.controller.js
@@ -1,3 +1,11 @@
-exports.get_department = (req, res, next) => {
+exports.get_departments = (req, res, next) => {
     res.render('checkDepartment');
 }
+
+exports.get_add_department = (req, res, next) => {
+    res.render('addDepartment');
+};
+
+exports.get_check_department = (req, res, next) => {
+    res.render('checkOneDpmt')
+};
--- navegacion/routes/deparment.routes.js
+++ navegacion/routes/deparment.routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const departmentController = require('../controllers/department.controller')
 
-router.get('/', departmentController.get_department)
+router.get('/check', departmentController.get_check_department);
+//router.get('/check/modify', departmentController);
+router.get('/add', departmentController.get_add_department);
+router.get('/', departmentController.get_departments);
 
 module.exports = router;
