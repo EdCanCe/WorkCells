af0b89eb52472202da8a4a0ccd6d2c1a2b772ed4
Mauricio Olguin <mauolguisanchez@gmail.com>
2025-03-07 22:56:52 -0600

    Finishing employee routes, wating for team check

--- navegacion/controllers/employee.controller.js
+++ navegacion/controllers/employee.controller.js
@@ -1,9 +1,23 @@
 
 
+exports.getAdd = (request, response, next) => {
+    response.render("employeeAdd");
+}
+
+exports.getModify = (request, response, next) => {
+    response.render("employeeCheckModify");
+}
+
+exports.getCheck = (request, response, next) => {
+    response.render("employeeCheck");
+}
+
+exports.getMe = (request, response, next) => {
+    response.render("employeeMe");
+}
 
 
 
-
-exports.get_root = (request, response, next) => {
+exports.getRoot = (request, response, next) => {
     response.render("employee");
 }
--- navegacion/routes/employee.routes.js
+++ navegacion/routes/employee.routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const employeeContrllers= require('../controllers/employee.controller');
-const { route } = require('./faults.routes');
 
 
-router.get('/', employeeContrllers.get_root);
+router.get('/add',employeeContrllers.getAdd);
+router.get('/check/modify',employeeContrllers.getModify);
+router.get('/check',employeeContrllers.getCheck);
+router.get('/me',employeeContrllers.getMe);
+router.get('/', employeeContrllers.getRoot);
 
 module.exports = router;
