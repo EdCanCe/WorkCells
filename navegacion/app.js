c8ac31ea25ae3b287b99841c8b88cc5fdcc90be5
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-03-07 21:01:51 -0600

    Refactor app.js

--- navegacion/app.js
+++ navegacion/app.js
@@ -4,16 +4,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const departmentRouter = require('./routes/department.routes');
-app.use('/department', departmentRouter);
 
 const homeRouter = require('./routes/home.routes');
 app.use('/home', homeRouter);
 app.use('/', homeRouter);
-const faults_routers = require("./routes/faults.routes");
-const absences_routers = require("./routes/absences.routes");
 
+const departmentRouter = require('./routes/department.routes');
+app.use('/department', departmentRouter);
+
+const faults_routers = require("./routes/faults.routes");
 app.use("/fault", faults_routers);
+
+const absences_routers = require("./routes/absences.routes");
 app.use("/absence", absences_routers);
 
 app.use((request, response, next) => {
