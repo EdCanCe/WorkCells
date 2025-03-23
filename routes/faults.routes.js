cf20e76d94679548d82767f63a4f7c4b98a00199
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-03-22 19:36:41 -0600

    Correccion de archivos

--- routes/faults.routes.js
+++ routes/faults.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const fault_controllers = require("../controllers/faults.controllers");
+const fault_controllers = require("../controllers/faults.controller");
 
 router.get("/add", fault_controllers.get_add);
 router.get("/check", fault_controllers.get_check);
-router.get("/",fault_controllers.get_root);
+router.get("/", fault_controllers.get_root);
 module.exports = router;
