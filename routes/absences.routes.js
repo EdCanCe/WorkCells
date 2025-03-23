f447114aeca9b3507a2a07011f627bb8eea7bfea
Manu <A01711581@tec.mx>
2025-03-22 19:58:06 -0600

    corrección de camelcase

--- routes/absences.routes.js
+++ routes/absences.routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const absences_controllers = require("../controllers/absences.controllers");
+const absencesControllers = require("../controllers/absences.controller");
 const isAuth = require('../util/is-auth');
 
-router.get("/aprove",isAuth, absences_controllers.get_aprove);
+router.get("/aprove",isAuth, absencesControllers.get_aprove);
 
-router.get("/add",isAuth, absences_controllers.get_add);
+router.get("/add",isAuth, absencesControllers.get_add);
 
-router.get("/check",isAuth, absences_controllers.get_check);
+router.get("/check",isAuth, absencesControllers.get_check);
 
-router.get("/", isAuth, absences_controllers.get_root);
+router.get("/", isAuth, absencesControllers.get_root);
 
 module.exports = router;
--- routes/department.routes.js
+++ routes/department.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const departmentController = require("../controllers/department.controller");
 const isAuth = require("../util/is-auth");
 
-router.get("/check",isAuth, departmentController.get_check_department);
-router.get("/check/modify",isAuth, departmentController.get_modify_department);
-router.get("/add",isAuth, departmentController.get_add_department);
-router.get("/", isAuth,departmentController.get_departments);
+router.get("/check",isAuth, departmentController.getCheckDepartment);
+router.get("/check/modify",isAuth, departmentController.getModifyDepartment);
+router.get("/add",isAuth, departmentController.getAddDepartment);
+router.get("/", isAuth,departmentController.getDepartments);
 
 module.exports = router;
