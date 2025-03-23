6605ac409b83ed4d8d34a810457eff6b97776315
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-03-23 17:52:24 -0600

    Se añade la funcionalidad para registrar usuario

---
+++ models/employee.model.js
@@ -0,0 +1,82 @@
+const db = require("../util/database"); // Importar la conexión
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = class Employee {
+  constructor(
+    curp,
+    rfc,
+    birthName,
+    surname,
+    mail,
+    zipCode,
+    houseNumber,
+    streetName,
+    colony,
+    workModality,
+    userRoleIDFK,
+    countryUserIDFK
+  ) {
+    this.curp = curp;
+    this.rfc = rfc;
+    this.birthName = birthName;
+    this.surname = surname;
+    this.mail = mail;
+    this.zipCode = zipCode;
+    this.houseNumber = houseNumber;
+    this.streetName = streetName;
+    this.colony = colony;
+    this.workModality = workModality;
+    this.userRoleIDFK = userRoleIDFK;
+    this.countryUserIDFK = countryUserIDFK;
+  }
+
+  save() {
+    const userID = uuidv4();
+    const passwd = "1234"; // Considera usar un hash para seguridad
+    const passwdFlag = false;
+    const workStatus = true;
+
+    const checkCURPQuery = `SELECT userID FROM user WHERE curp = ?`;
+
+    return db
+      .execute(checkCURPQuery, [this.curp])
+      .then(([rows]) => {
+        if (rows.length > 0) {
+          // El usuario ya existe, evitar el registro
+          throw new Error("El usuario con este CURP ya está registrado.");
+        }
+
+        // Si no existe, proceder con la inserción
+        const query = `
+          INSERT INTO user(
+            userID, curp, rfc, birthName, surname, mail, passwd, passwdFlag, zipCode, houseNumber, 
+            streetName, colony, workModality, workStatus, userRoleIDFK, countryUserIDFK
+          ) 
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        return db.execute(query, [
+          userID,
+          this.curp,
+          this.rfc,
+          this.birthName,
+          this.surname,
+          this.mail,
+          passwd,
+          passwdFlag,
+          this.zipCode,
+          this.houseNumber,
+          this.streetName,
+          this.colony,
+          this.workModality,
+          workStatus,
+          this.userRoleIDFK,
+          this.countryUserIDFK,
+        ]);
+      })
+      .catch((error) => {
+        console.error("Error al guardar el usuario:", error.message);
+        throw error;
+      });
+  }
+};
