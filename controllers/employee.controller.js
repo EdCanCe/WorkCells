be5af72b21f033eef7128bd5ff0bf834ade3bd71
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-03-23 17:52:24 -0600

    Se añade la funcionalidad para registrar usuario

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,23 +1,61 @@
-
+const Employee = require("../models/employee.model");
 
 exports.getAdd = (request, response, next) => {
-    response.render("employeeAdd");
-}
+  response.render("employeeAdd", {
+    isLoggedIn: request.session.isLoggedIn || false,
+    info: request.session.info || "",
+    csrfToken: request.csrfToken(),
+  });
+};
+
+exports.postAdd = (request, response, next) => {
+  const employee = new Employee(
+    request.body.curp,
+    request.body.rfc,
+    request.body.birthName,
+    request.body.surname,
+    request.body.mail,
+    request.body.zipCode,
+    request.body.houseNumber,
+    request.body.streetName,
+    request.body.colony,
+    request.body.workModality,
+    request.body.userRoleIDFK,
+    request.body.countryUserIDFK
+  );
+  employee
+    .save()
+    .then(() => {
+      request.session.info = `Employee created`;
+      response.redirect("/employee");
+    })
+    .catch((error) => {
+      console.error(error); // Mejor manejo de error
+      response.status(500).send("Error al guardar los datos.");
+    });
+};
 
 exports.getModify = (request, response, next) => {
-    response.render("employeeCheckModify");
-}
+  response.render("employeeCheckModify");
+};
 
 exports.getCheck = (request, response, next) => {
-    response.render("employeeCheck");
-}
+  response.render("employeeCheck");
+};
 
 exports.getMe = (request, response, next) => {
-    response.render("employeeMe");
-}
-
-
+  response.render("employeeMe");
+};
 
 exports.getRoot = (request, response, next) => {
-    response.render("employee");
-}
+  const mensaje = request.session.info || "";
+
+  // Limpiar la sesión después de usar el mensaje
+  request.session.info = "";
+
+  response.render("employee", {
+    isLoggedIn: request.session.isLoggedIn || false,
+    username: request.session.username || "",
+    info: mensaje, // Aquí pasamos el mensaje de éxito o error
+  });
+};
