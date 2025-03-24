b729844b905f7c2e1b439e57a1e5e34046817369
YaelChar417 <A01711111@tec.mx>
2025-03-24 13:24:12 -0600

    Update: Formato para absences controller y model

--- controllers/absences.controller.js
+++ controllers/absences.controller.js
@@ -1,4 +1,4 @@
-const Absence = require('../models/absence.model');
+const Absence = require("../models/absence.model");
 
 exports.get_check = (request, response, next) => {
   response.render("absence_check");
@@ -15,61 +15,61 @@ exports.get_add = (request, response, next) => {
       response.render("absences_add", {
         csrfToken: request.csrfToken(),
         absences: absences,
-        info: request.session.info || '',
+        info: request.session.info || "",
       });
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err);
     });
-  
 };
 
 exports.post_add = (req, res, next) => {
   console.log(req.body);
-  Absence.getID(req.session.mail)
-    .then(([rows]) => {
-      if(rows.length == 0)
-      {
-        res.send(500);
-      }
-      const userID = rows[0].userID;
-      const absence = new Absence(req.body.startDate, req.body.endDate, req.body.reason, userID);
-      absence.save()
-        .then(() => {
-          req.session.info = `Absence from ${absence.startDate} to ${absence.endDate} created`
-          res.redirect('/absence');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  Absence.getID(req.session.mail).then(([rows]) => {
+    if (rows.length == 0) {
+      res.send(500);
+    }
+    const userID = rows[0].userID;
+    const absence = new Absence(
+      req.body.startDate,
+      req.body.endDate,
+      req.body.reason,
+      userID
+    );
+    absence
+      .save()
+      .then(() => {
+        req.session.info = `Absence from ${absence.startDate} to ${absence.endDate} created`;
+        res.redirect("/absence");
+      })
+      .catch((err) => {
+        console.log(err);
       });
+  });
 };
 
 exports.get_root = (req, res, next) => {
-  const mensaje = req.session.info || '';
-  if(req.session.info)
-  {
-    req.session.info = '';
+  const mensaje = req.session.info || "";
+  if (req.session.info) {
+    req.session.info = "";
   }
   console.log(req.session.mail);
-  Absence.getID(req.session.mail)
-    .then(([rows]) => {
-      if(rows.length == 0)
-        {
-          res.send(500);
-        }
-        const userID = rows[0].userID;
-        Absence.fetchAllByID(userID)
-          .then(([rows, fieldData]) => {
-            console.log(fieldData);
-            console.log(rows);
-            res.render('absences_list', {
-              absences: rows,
-              info: mensaje,
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    })
+  Absence.getID(req.session.mail).then(([rows]) => {
+    if (rows.length == 0) {
+      res.send(500);
+    }
+    const userID = rows[0].userID;
+    Absence.fetchAllByID(userID)
+      .then(([rows, fieldData]) => {
+        console.log(fieldData);
+        console.log(rows);
+        res.render("absences_list", {
+          absences: rows,
+          info: mensaje,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
--- models/absence.model.js
+++ models/absence.model.js
@@ -1,58 +1,62 @@
-const db = require('../util/database');
-const { v4: uuidv4 } = require('uuid');
+const db = require("../util/database");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = class Absence {
-    constructor(startDate, endDate, reason, absenceUserID){
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.reason = reason;
-        this.justified = 0; // 0 -> no justificada | 1 -> justificada
-        this.absenceUserID = absenceUserID;
-    }
+  constructor(startDate, endDate, reason, absenceUserID) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.reason = reason;
+    this.justified = 0; // 0 -> no justificada | 1 -> justificada
+    this.absenceUserID = absenceUserID;
+  }
 
-    save(){
-        const absenceID = uuidv4();
-        return db.execute(`INSERT INTO absence(absenceID, startDate, endDate, 
-                reason, justified, absenceUserIDFK) VALUES(?,?,?,?,?,?)`,[absenceID, 
-                this.startDate, this.endDate, this.reason, 0, this.absenceUserID]);
-    }
+  save() {
+    const absenceID = uuidv4();
+    return db.execute(
+      `INSERT INTO absence(absenceID, startDate, endDate, 
+                reason, justified, absenceUserIDFK) VALUES(?,?,?,?,?,?)`,
+      [
+        absenceID,
+        this.startDate,
+        this.endDate,
+        this.reason,
+        0,
+        this.absenceUserID,
+      ]
+    );
+  }
 
-    static fetchAll(){
-        return db.execute('SELECT * FROM absence ORDER BY startDate DESC LIMIT 10');
-    }
+  static fetchAll() {
+    return db.execute("SELECT * FROM absence ORDER BY startDate DESC LIMIT 10");
+  }
 
-    static fetchAllByID(id){
-        return db.execute('SELECT * FROM absence WHERE absenceUserIDFK = ?', [id]);
-    }
+  static fetchAllByID(id) {
+    return db.execute("SELECT * FROM absence WHERE absenceUserIDFK = ?", [id]);
+  }
 
-    static fetchOne(id){
-        return db.execute('SELECT * FROM absence WHERE absenceID = ?', [id]);
-    }
+  static fetchOne(id) {
+    return db.execute("SELECT * FROM absence WHERE absenceID = ?", [id]);
+  }
 
-    static fetch(id){
-        if(id)
-        {
-            return this.fetchOne(id);
-        }else
-        {
-            return this.fetchAll();
-        }
+  static fetch(id) {
+    if (id) {
+      return this.fetchOne(id);
+    } else {
+      return this.fetchAll();
     }
+  }
 
-    static getID(email){
-        return db.execute(`SELECT userID FROM user WHERE mail = ?`, [email])
-    }
+  static getID(email) {
+    return db.execute(`SELECT userID FROM user WHERE mail = ?`, [email]);
+  }
 
-    static fetchByDateType(startDate, endDate, userID) {
-        return db.execute(`(SELECT * FROM absence WHERE startDate BETWEEN ? AND ? AND absenceUserIDFK = ?)
+  static fetchByDateType(startDate, endDate, userID) {
+    return db.execute(
+      `(SELECT * FROM absence WHERE startDate BETWEEN ? AND ? AND absenceUserIDFK = ?)
     UNION
-    (SELECT * FROM absence WHERE startDate BETWEEN ? AND ? AND absenceUserIDFK = ?)`, [startDate, endDate, userID, startDate, endDate, userID]
-        );
-      }
+    (SELECT * FROM absence WHERE startDate BETWEEN ? AND ? AND absenceUserIDFK = ?)`,
+      [startDate, endDate, userID, startDate, endDate, userID]
+    );
+  }
 };
 const db = require("../util/database"); // Asegúrate de importar tu módulo de conexión
-
-class Absence {
-  
-
-module.exports = Absence;
