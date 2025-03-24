784e2f7f462664a81290a833f4132472be496a7c
Manu <A01711581@tec.mx>
2025-03-24 13:02:15 -0600

    push antes de hacer merge con develop

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const session = require('express-session');
+const session = require("express-session");
 
-app.use(session({
-    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
-    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret:
+      "mi string secreto que debe ser un string aleatorio muy largo, no como éste",
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
-}));
+  })
+);
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const csrf = require('csurf');
-const csrfProtection = csrf(); 
-app.use(csrfProtection); 
+const csrf = require("csurf");
+const csrfProtection = csrf();
+app.use(csrfProtection);
 
-const usersRoutes = require('./routes/user.routes');
-app.use('/login', usersRoutes);
-const homeRouter = require('./routes/home.routes');
-app.use('/home', homeRouter);
-app.use('/', homeRouter);
+const usersRoutes = require("./routes/user.routes");
+app.use("/login", usersRoutes);
+const homeRouter = require("./routes/home.routes");
+app.use("/home", homeRouter);
+app.use("/", homeRouter);
 
 const calendarRouter = require("./routes/calendar.routes");
 app.use("/calendar", calendarRouter);
 
-const departmentRouter = require('./routes/department.routes');
-app.use('/department', departmentRouter);
+const departmentRouter = require("./routes/department.routes");
+app.use("/department", departmentRouter);
 
 const faults_routers = require("./routes/faults.routes");
 const absences_routers = require("./routes/absences.routes");
-const employeeRouter = require('./routes/employee.routes');
+const employeeRouter = require("./routes/employee.routes");
 const vacationRouter = require("./routes/vacation.routes");
 
-
-app.use('/department', departmentRouter);
-app.use('/home', homeRouter);
+app.use("/department", departmentRouter);
+app.use("/home", homeRouter);
 app.use("/fault", faults_routers);
-app.use('/absence', absences_routers);
-app.use('/employee', employeeRouter);
+app.use("/absence", absences_routers);
+app.use("/employee", employeeRouter);
 app.use("/vacation", vacationRouter);
-
 
 app.use("/", homeRouter);
 const holidayRouter = require("./routes/holiday.routes.js");
@@ -55,7 +59,7 @@ app.use("/oneToOne", oneToOneRouter);
 
 app.use((request, response, next) => {
   response.statusCode = 404;
-  response.render('notFound');
+  response.render("notFound");
 });
 
 app.listen(3000);
