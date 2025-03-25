73a02d995e2921efa62b3f7f4c27b9db5743e66a
Yael Charles <yaelcharles417@gmail.com>
2025-03-24 21:52:20 -0600

    Feature: modelo creado para absenceMedia, cargar archivos

---
+++ models/absenceMedia.mode.js
@@ -0,0 +1,13 @@
+const db = require('../util/database');
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = class AbsenceMedia{
+    constructor(mediaLink, absenceIDFK){
+        this.mediaLink = mediaLink;
+        this.absenceIDFK = absenceIDFK;
+    }
+
+    save(){
+        const absenceMediaID = uuidv4();
+    }
+}
