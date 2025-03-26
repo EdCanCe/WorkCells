2a687aec943b267444038013361707756667cca6
Yael Charles <yaelcharles417@gmail.com>
2025-03-25 20:58:05 -0600

    Feature: estructura template del reporte de rotacion

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -1,3 +1,7 @@
 exports.getRoot = (request, response, next) => {
     response.render("reportsMenu");
 };
+
+exports.getEmployeeRotation = (request, response, next) => {
+    response.render("reportRotation");
+};
--- routes/report.routes.js
+++ routes/report.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const reportControllers = require("../controllers/report.controller");
 const isAuth = require("../util/is-auth");
 
+router.get("/staffRotation", isAuth, reportControllers.getEmployeeRotation);
 router.get("/", isAuth, reportControllers.getRoot);
 
 module.exports = router;
