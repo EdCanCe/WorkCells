0e3defdb00bf6193f989c925f6b8d64a822b62bb
Yael Charles <yaelcharles417@gmail.com>
2025-03-26 16:57:33 -0600

    Fix: requisitos de yael hechos quinonez

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'workcells',
-    password: '1234',
+    host: "localhost",
+    user: "root",
+    database: "workcells",
+    password: "",
 });
 
 module.exports = pool.promise();
