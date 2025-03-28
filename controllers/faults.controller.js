98d0e8e23fb0be1a972e4dc86823376355c06524
Manu <A01711581@tec.mx>
2025-03-27 21:24:45 -0600

    commit antes de pull

--- controllers/faults.controller.js
+++ controllers/faults.controller.js
@@ -2,85 +2,87 @@ const Fault = require("../models/faults.model");
 const FaultMedia = require("../models/faultsMedia.model");
 
 exports.getAdd = (request, response, next) => {
-    response.render("faultsAdd", {
-        isLoggedIn: request.session.isLoggedIn || false,
-        info: request.session.info || "",
-        csrfToken: request.csrfToken(),
-    });
+  response.render("faultsAdd", {
+    isLoggedIn: request.session.isLoggedIn || false,
+    info: request.session.info || "",
+    csrfToken: request.csrfToken(),
+  });
 };
 
 exports.postAdd = (request, response, next) => {
-    console.log(request.body); // Verifica que los datos lleguen correctamente
+  console.log(request.file);
+  console.log(request.body); // Verifica que los datos lleguen correctamente
 
-    // Validación de valores en el cuerpo de la solicitud
+  if (request.file != ".pdf" || request.file != "jpng" || request.file != "")
     if (!request.body.reason || !request.body.doneDate || !request.body.email) {
-        return response.redirect("/error"); // Redirigir a una página de error si faltan datos
+      // Validación de valores en el cuerpo de la solicitud
+      return response.redirect("/error"); // Redirigir a una página de error si faltan datos
     }
 
-    // Crear un nuevo objeto Fault
-    const faults = new Fault(
-        request.body.reason,
-        request.body.doneDate,
-        request.body.email
-    );
+  // Crear un nuevo objeto Fault
+  const faults = new Fault(
+    request.body.reason,
+    request.body.doneDate,
+    request.body.email
+  );
 
-    faults
-        .save()
-        .then((faultID) => {
-            request.session.info = `Fault of ${faults.email} created`;
+  faults
+    .save()
+    .then((faultID) => {
+      request.session.info = `Fault of ${faults.email} created`;
 
-            if (request.file) {
-                const media = new FaultMedia(request.file.filename, faultID);
-                return media.save();
-            }
-            return Promise.resolve();
-        })
-        .then(() => {
-            response.redirect("/fault");
-        })
-        .catch((error) => {
-            console.error(error);
-            request.session.info = `Error al ingresar datos.`;
-            response.redirect("/fault");
-            response.status(500);
-        });
+      if (request.file) {
+        const media = new FaultMedia(request.file.filename, faultID);
+        return media.save();
+      }
+      return Promise.resolve();
+    })
+    .then(() => {
+      response.redirect("/fault");
+    })
+    .catch((error) => {
+      console.error(error);
+      request.session.info = `Error al ingresar datos.`;
+      response.redirect("/fault");
+      response.status(500);
+    });
 };
 
 exports.getCheck = (request, response, next) => {
-    response.render("check_fault");
+  response.render("check_fault");
 };
 
 exports.getRoot = (request, response, next) => {
-    const mensaje = request.session.info || "";
+  const mensaje = request.session.info || "";
 
-    // Limpiar la sesión después de usar el mensaje
-    request.session.info = "";
+  // Limpiar la sesión después de usar el mensaje
+  request.session.info = "";
 
-    Fault.fetchAll()
-        .then(([rows, fieldData]) => {
-            response.render("faults", {
-                isLoggedIn: request.session.isLoggedIn || false,
-                username: request.session.username || "",
-                fault: rows,
-                info: mensaje,
-            });
-        })
-        .catch((error) => {
-            console.error(error); // Mejor manejo de error
-            response.status(500).send("Error al obtener los datos.");
-        });
+  Fault.fetchAll()
+    .then(([rows, fieldData]) => {
+      response.render("faults", {
+        isLoggedIn: request.session.isLoggedIn || false,
+        username: request.session.username || "",
+        fault: rows,
+        info: mensaje,
+      });
+    })
+    .catch((error) => {
+      console.error(error); // Mejor manejo de error
+      response.status(500).send("Error al obtener los datos.");
+    });
 };
 
 exports.listPaginated = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = 10;
-    const offset = (page - 1) * limit;
+  const page = parseInt(req.query.page) || 1;
+  const limit = 10;
+  const offset = (page - 1) * limit;
 
-    try {
-        const [rows] = await Fault.getFaltasPaginated(limit, offset); // <== AQUÍ EL CAMBIO
-        res.json(rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Error al obtener las faltas" });
-    }
+  try {
+    const [rows] = await Fault.getFaltasPaginated(limit, offset); // <== AQUÍ EL CAMBIO
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al obtener las faltas" });
+  }
 };
