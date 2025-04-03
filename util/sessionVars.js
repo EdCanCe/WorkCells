0d07aa4e00f05fbcad90ee3f37ffa91fc4c25bf5
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-04-03 01:38:00 -0600

    Add: Módulo para funcionamiento de estados

--- util/sessionVars.js
+++ util/sessionVars.js
@@ -6,13 +6,17 @@
  * @returns session variables
  */
 module.exports = (request) => {
+    // Obtiene un popup de alerta, lo guarda y lo resetea
+    const alert = request.session.alert || "";
+    if (request.session.alert) request.session.alert = "";
+
     // Obtiene un popup de warning, lo guarda y lo resetea
     const warning = request.session.warning || "";
     if (request.session.warning) request.session.warning = "";
 
     // Obtiene un popup de info, lo guarda y lo resetea
     const info = request.session.info || "";
-    if (request.session.info) request.session.warning = "";
+    if (request.session.info) request.session.info = "";
 
     return {
         csrfToken: request.csrfToken(),
@@ -22,6 +26,7 @@ module.exports = (request) => {
         role: request.session.role || "none",
         privilegios: request.session.privilegios || [],
         navIsOpen: (request.cookies.navOpened === '0' ? false : true),
+        alert: alert,
         warning: warning,
         info: info,
     };
