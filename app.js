acfe747a3037268f9855dbe049c1e7fb54d458fd
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-04-04 10:36:44 -0600

    Add: Timout de sesión

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use(
         secret: crypto.randomUUID(),
         resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
         saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+        rolling: true,
+        cookie: {
+            maxAge: 15 * 60 * 1000 // 15 minutos en milisegundos
+        },
     })
 );
 
