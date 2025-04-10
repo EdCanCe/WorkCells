6e5821f9931292aa545fd59f700a11e05b423187
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-04-10 10:00:12 -0600

    Correccion de database.js

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "workcells",
-    password: "",
+    host: process.env.DATABASEIP,
+    port: 3306,
+    user: process.env.DATABASEUSER,
+    database: process.env.DATABASEDB,
+    password: process.env.DATABASEPW,
 });
 
 module.exports = pool.promise();
