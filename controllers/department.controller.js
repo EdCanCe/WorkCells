38c6c68616a86c3ce0621f161f7957237cfc80d7
YaelChar417 <A01711111@tec.mx>
2025-04-09 20:39:01 -0600

    Feature: validación en controller para gestionar vistas de departamento entre lider y superadmin

--- controllers/department.controller.js
+++ controllers/department.controller.js
@@ -1,25 +1,35 @@
-const sessionVars = require('../util/sessionVars');
+const { session } = require("passport");
+const sessionVars = require("../util/sessionVars");
 
 exports.getDepartments = (request, response, next) => {
-    response.render('checkDepartment', {
-        ...sessionVars(request),
-    });
-}
+    const role = sessionVars(request).role;
+
+    // vista del lider para ver su departamento
+    if (role === "Department Leader") {
+        response.send("Vista del lider");
+    }
+    // vista del superadmin, modificar en un futuro
+    else {
+        response.render("checkDepartment", {
+            ...sessionVars(request),
+        });
+    }
+};
 
 exports.getAddDepartment = (request, response, next) => {
-    response.render('addDepartment', {
+    response.render("addDepartment", {
         ...sessionVars(request),
     });
 };
 
 exports.getCheckDepartment = (request, response, next) => {
-    response.render('checkOneDpmt', {
+    response.render("checkOneDpmt", {
         ...sessionVars(request),
-    })
+    });
 };
 
 exports.getModifyDepartment = (request, response, next) => {
-    response.render('modifyDepartment', {
+    response.render("modifyDepartment", {
         ...sessionVars(request),
-    })
+    });
 };
