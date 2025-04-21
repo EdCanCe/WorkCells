0546f5f42c7add15a71eb7d59a02f07a59e988dd
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-04-21 15:39:41 -0600

    Se crea el modelo de las graficas de las respuestas de One On One por departamento, realizado con pair programming con Manuel Bajos

--- models/report.model.js
+++ models/report.model.js
@@ -70,4 +70,78 @@ module.exports = class Report {
             [start, end]
         );
     }
+
+    static fetchDepartment() {
+        return db.execute(`SELECT d.departmentID, d.title AS departmentTitle, e.title AS enterpriseTitle 
+                            FROM department d, enterprise e 
+                            WHERE d.enterpriseIDFK = e.enterpriseID;`);
+    }
+
+    static getAnswerWorkload(department) {
+        return db.execute(
+            `SELECT m.evaluation, o.summary, d.title AS 'Departamento', e.title AS 'Empresa', l.birthName
+            FROM oneOnOneMeasure m, oneOnOneMeasurable o, oneOnOne n, user u, department d, enterprise e, user l
+            WHERE m.measurableIDFK = o.measurableID 
+            AND m.measureOneOnOneIDFK = n.oneOnOneID
+            AND n.oneOnOneUserIDFK = u.userID
+            AND u.prioritaryDepartmentIDFK = d.departmentID
+            AND d.enterpriseIDFK = e.enterpriseID
+            AND d.departmentLeaderIDFK = l.userID
+            AND m.measurableIDFK = '15125ff3-f1c8-4260-872a-fdea4e7592a5'
+            AND d.departmentID = ?
+            ORDER BY e.title;`,
+            [department]
+        );
+    }
+
+    static getAnswerPhysicalHealth(department) {
+        return db.execute(
+            `SELECT m.evaluation, o.summary, d.title AS 'Departamento', e.title AS 'Empresa', l.birthName
+            FROM oneOnOneMeasure m, oneOnOneMeasurable o, oneOnOne n, user u, department d, enterprise e, user l
+            WHERE m.measurableIDFK = o.measurableID 
+            AND m.measureOneOnOneIDFK = n.oneOnOneID
+            AND n.oneOnOneUserIDFK = u.userID
+            AND u.prioritaryDepartmentIDFK = d.departmentID
+            AND d.enterpriseIDFK = e.enterpriseID
+            AND d.departmentLeaderIDFK = l.userID
+            AND m.measurableIDFK = '5bb474dc-76c5-4561-84a6-5ef52c440a3e'
+            AND d.departmentID = ?
+            ORDER BY e.title;`,
+            [department]
+        );
+    }
+
+    static getAnswerAcknowledgement(department) {
+        return db.execute(
+            `SELECT m.evaluation, o.summary, d.title AS 'Departamento', e.title AS 'Empresa', l.birthName
+            FROM oneOnOneMeasure m, oneOnOneMeasurable o, oneOnOne n, user u, department d, enterprise e, user l
+            WHERE m.measurableIDFK = o.measurableID 
+            AND m.measureOneOnOneIDFK = n.oneOnOneID
+            AND n.oneOnOneUserIDFK = u.userID
+            AND u.prioritaryDepartmentIDFK = d.departmentID
+            AND d.enterpriseIDFK = e.enterpriseID
+            AND d.departmentLeaderIDFK = l.userID
+            AND m.measurableIDFK = 'ea9d0fd3-2bed-4b2e-9de8-1e297982d080'
+            AND d.departmentID = ?
+            ORDER BY e.title;`,
+            [department]
+        );
+    }
+
+    static getAnswerWorkLifeBalance(department) {
+        return db.execute(
+            `SELECT m.evaluation, o.summary, d.title AS 'Departamento', e.title AS 'Empresa', l.birthName
+            FROM oneOnOneMeasure m, oneOnOneMeasurable o, oneOnOne n, user u, department d, enterprise e, user l
+            WHERE m.measurableIDFK = o.measurableID 
+            AND m.measureOneOnOneIDFK = n.oneOnOneID
+            AND n.oneOnOneUserIDFK = u.userID
+            AND u.prioritaryDepartmentIDFK = d.departmentID
+            AND d.enterpriseIDFK = e.enterpriseID
+            AND d.departmentLeaderIDFK = l.userID
+            AND m.measurableIDFK = 'fd306cbf-90f3-498d-9e37-0370ddf0cb91'
+            AND d.departmentID = ?
+            ORDER BY e.title;`,
+            [department]
+        );
+    }
 };
