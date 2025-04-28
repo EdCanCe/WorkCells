1f7db77aadd1915c67cfb5a83f3258778264a4f0
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-04-28 10:33:09 -0600

    Merge con develop

--- util/google-auth.js
+++ util/google-auth.js
@@ -8,7 +8,7 @@ passport.use(
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
             // Se agrega /auth/google antes de la ruta de inicio
-            callbackURL: "http://tec1.nuclea.solutions/google/callback",
+            callbackURL: "https://tec1.nuclea.solutions/google/callback",
             passReqToCallback: true,
         },
         async (request, accessToken, refreshToken, profile, cb) => {
